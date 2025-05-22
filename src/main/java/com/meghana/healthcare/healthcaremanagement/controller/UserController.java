50b2f9ca5010edf2b2d0175a6e19af2f981150b6
Meghana0303 <beesas1@udayton.edu>
2025-05-22 14:32:30 -0400

    Add UserController for creating and fetching users via REST API

---
+++ src/main/java/com/meghana/healthcare/healthcaremanagement/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.meghana.healthcare.healthcaremanagement.controller;
+
+import com.meghana.healthcare.healthcaremanagement.entity.User;
+import com.meghana.healthcare.healthcaremanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Create a new user
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // Get all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
