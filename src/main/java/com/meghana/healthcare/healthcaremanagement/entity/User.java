d75ec91cf8276ad097476f284cae13f0ed23aa91
Meghana0303 <beesas1@udayton.edu>
2025-05-22 14:26:23 -0400

    Add User entity with JPA annotations and database config

---
+++ src/main/java/com/meghana/healthcare/healthcaremanagement/entity/User.java
@@ -0,0 +1,21 @@
+package com.meghana.healthcare.healthcaremanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role; // e.g., PATIENT, DOCTOR, ADMIN
+}
+
